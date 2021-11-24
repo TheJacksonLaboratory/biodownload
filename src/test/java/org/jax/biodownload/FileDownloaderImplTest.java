1a734f97368dc70511333cdc44052d4e859b096b
Baha El Kassaby <Baha.ElKassaby@jax.org>
2021-11-24 09:50:55 -0500

    Fix POM file and disable tests
    *put junit version
    *define build goal
    *disable the tests

--- src/test/java/org/jax/biodownload/FileDownloaderImplTest.java
+++ src/test/java/org/jax/biodownload/FileDownloaderImplTest.java
@@ -1,6 +1,7 @@
 package org.jax.biodownload;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class FileDownloaderImplTest {
@@ -13,12 +14,14 @@ public class FileDownloaderImplTest {
     }
 
     @Test
+    @Disabled
     public void testHpoDownload() {
         // TODO
     }
 
 
     @Test
+    @Disabled
     public void testGoDownload() {
         // TODO
     }
